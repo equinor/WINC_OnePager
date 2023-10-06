5cf280fa06c651a4c07342762bc9ee29d88c093b
hzh <hzh@equinor.com>
2023-10-06 02:05:38 +0200

    start to add unit testings

---
+++ tests/conftest.py
@@ -0,0 +1,100 @@
+""" define some fixtures
+"""
+import pathlib
+import json
+import pickle
+
+import pytest
+
+from src.WellClass.libs.utils import (
+    csv_parser,
+    yaml_parser,
+)
+
+from src.WellClass.libs.well_class import Well
+
+from src.WellClass.libs.grid_utils import (
+    WellDataFrame,
+)
+
+# a test example
+example = {
+    'sim_path': './test_data/examples/smeaheia_v1',
+    'well_config': 'smeaheia.yaml',
+    'sim_case': 'GEN_NOLGR_PH2.EGRID'
+}
+
+@pytest.fixture(scope='session')
+def well_class_fixture():
+    """ fixture for WellClass
+    """
+
+    # the paths
+    sim_path = pathlib.Path(example['sim_path'])
+    well_name = sim_path/example['well_config']
+
+    # extract suffix
+    suffix = well_name.suffix
+    # .yaml or .csv?
+    use_yaml = suffix in ['.yaml', '.yml']
+
+    if use_yaml:
+        
+        # # pydantic model
+        well_model = yaml_parser(well_name)
+        well_csv = json.loads(well_model.spec.model_dump_json())
+    else:
+
+        # load the well information
+        well_csv = csv_parser(well_name)
+
+    # instantiate class
+    my_well = Well(header       = well_csv['well_header'], 
+                   drilling     = well_csv['drilling'],
+                   casings      = well_csv['casing_cement'],
+                   geology      = well_csv['geology'],
+                   barriers     = well_csv['barriers'], 
+                   barrier_perm = well_csv['barrier_permeability'],
+                   co2_datum    = well_csv['co2_datum'],
+                )
+    
+    return my_well
+
+@pytest.fixture(scope='session')
+def well_class_dict_fixture():
+    """ fixture for loading WellClass .pkl file for unit testing
+    """
+
+    # the paths
+    sim_path = pathlib.Path(example['sim_path'])
+    well_name = sim_path/example['well_config']
+
+    # extract suffix
+    suffix = well_name.suffix
+    # .yaml or .csv?
+    use_yaml = suffix in ['.yaml', '.yml']
+
+    # only handle .yaml file
+    assert use_yaml is True
+
+    # convert .yaml file suffix to .pkl
+    well_name_pkl = well_name.with_suffix('.pkl')
+
+    # load .pkl file
+    with open(well_name_pkl, 'rb') as f:
+        my_well = f.read()
+
+    # convert it to dictionary
+    my_well_dict = pickle.loads(my_well)
+
+    return my_well_dict
+
+@pytest.fixture(scope='session')
+def well_dataframe_fixture(well_class_fixture):
+    """ Well DataFrame fixture
+        Args:
+            well_class_fixture: fixture for WellClass
+    """
+    well_df = WellDataFrame(well_class_fixture)
+
+    return well_df
---
+++ tests/well_class/test_well_class.py
@@ -0,0 +1,83 @@
+
+
+def test_well_header(well_class_fixture,
+                     well_class_dict_fixture) -> None:
+    """ test well_header
+        Args:
+            well_class_fixture: fixture for WellClass
+            well_class_dict_fixture: fixture for WellClass example
+    """
+
+    info    = well_class_fixture.header
+    info_gt = well_class_dict_fixture['header']
+
+    assert info['well_name'] == info_gt['well_name']
+    assert info['well_td_rkb'] == info_gt['well_td_rkb']
+
+    # compare the whole dictionary
+    assert info == info_gt
+
+def test_drilling(well_class_fixture,
+                  well_class_dict_fixture) -> None:
+    """ test drilling
+        Args:
+            well_class_fixture: fixture for WellClass
+            well_class_dict_fixture: fixture for WellClass example
+    """
+
+    info = well_class_fixture.drilling
+    info_gt = well_class_dict_fixture['drilling']
+
+    # section index
+    sec_index = 1
+
+    assert info['diameter_in'][sec_index] == info_gt['diameter_in'][sec_index]
+    assert info['top_rkb'][sec_index] == info_gt['top_rkb'][sec_index]
+    assert info['bottom_rkb'][sec_index] == info_gt['bottom_rkb'][sec_index]
+
+    # compare the whole dictionary
+    assert info == info_gt
+
+def test_casings(well_class_fixture,
+                 well_class_dict_fixture) -> None:
+    """ test casings
+        Args:
+            well_class_fixture: fixture for WellClass
+            well_class_dict_fixture: fixture for WellClass example
+    """
+
+    info = well_class_fixture.casings
+    info_gt = well_class_dict_fixture['casings']
+
+    # section index
+    sec_index = 1
+
+    assert info['diameter_in'][sec_index] == info_gt['diameter_in'][sec_index]
+    assert info['top_rkb'][sec_index] == info_gt['top_rkb'][sec_index]
+    assert info['bottom_rkb'][sec_index] == info_gt['bottom_rkb'][sec_index]
+
+    # compare the whole dictionary
+    assert info == info_gt
+
+def test_barriers(well_class_fixture,
+                  well_class_dict_fixture) -> None:
+    """ test barriers
+        Args:
+            well_class_fixture: fixture for WellClass
+            well_class_dict_fixture: fixture for WellClass example
+    """
+
+    info = well_class_fixture.barriers
+    info_gt = well_class_dict_fixture['barriers']
+
+    # section index
+    sec_index = 1
+
+    assert info['barrier_name'][sec_index] == info_gt['barrier_name'][sec_index]
+    assert info['top_rkb'][sec_index] == info_gt['top_rkb'][sec_index]
+    assert info['bottom_rkb'][sec_index] == info_gt['bottom_rkb'][sec_index]
+
+    # compare the whole dictionary
+    assert info == info_gt
+    
+
