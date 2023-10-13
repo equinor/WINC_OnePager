c3c2c367058c9cde8051541604ce3a49d313f68c
hzh <hzh@equinor.com>
2023-10-13 15:55:45 +0200

    split out computing bbox

--- src/WellClass/libs/grid_utils/grid_refine_base.py
+++ src/WellClass/libs/grid_utils/grid_refine_base.py
@@ -6,10 +6,7 @@ import pandas as pd
 
 from .grid_coarse import GridCoarse
 
-from .LGR_bbox import (
-    get_ij_indices,
-    get_k_indices,
-)
+from .LGR_bbox import compute_bbox
 
 class GridRefineBase:
 
@@ -242,89 +239,14 @@ class GridRefineBase:
         mesh_df = self.mesh_df
         nxy = self.nx
 
+        # depth cut off
+        maxDepth = mesh_df['Zcorn_bottom'].max()
+
         # ### 1. Drillings
-        # z
-        drilling_df['k_min'] = -1
-        drilling_df['k_max'] = -1
-        # xy
-        drilling_df['ij_min'] = -1
-        drilling_df['ij_max'] = -1
-
-        for idx, row in drilling_df.iterrows():
-            
-            top, bottom = row['top_msl'], row['bottom_msl']
-
-            if top < mesh_df['Zcorn_bottom'].max():
-                
-                # k ranges
-                k_min, k_max = get_k_indices(mesh_df, top, bottom)
-
-                # x-y ranges
-                ij_min, ij_max = get_ij_indices(nxy, row['n_grd_id'])
-
-                # to dataframe
-                drilling_df.loc[idx, 'k_min'] = k_min
-                drilling_df.loc[idx, 'k_max'] = k_max
-                drilling_df.loc[idx, 'ij_min'] = ij_min
-                drilling_df.loc[idx, 'ij_max'] = ij_max# # Bounding box for well elements
+        compute_bbox(mesh_df, drilling_df, nxy=nxy, maxDepth=maxDepth)
 
         # ### 2. Casings
-
-        # casing, k
-        casings_df['k_min'] = -1
-        casings_df['k_max'] = -1
-        # cement bond, k
-        casings_df['toc_k_min'] = -1
-        casings_df['toc_k_max'] = -1
-        # casing, xy
-        casings_df['ij_min'] = -1
-        casings_df['ij_max'] = -1
-
-        for idx, row in casings_df.iterrows():
-
-            # A) casing, z ranges
-            top, bottom  = row['top_msl'], row['bottom_msl']
-            
-            # convert to indices
-            k_min, k_max = get_k_indices(mesh_df, top, bottom)
-            
-            # B) cement, z ranges
-            toc, boc = row['toc_msl'], row['boc_msl']
-
-            # convert to indices
-            toc_k_min, toc_k_max = get_k_indices(mesh_df, toc, boc)
-            
-            # C) xy ranges
-            ij_min, ij_max = get_ij_indices(nxy, row['n_grd_id'])
-            
-            # to dataframe
-            casings_df.loc[idx, 'k_min'] = k_min
-            casings_df.loc[idx, 'k_max'] = k_max
-            casings_df.loc[idx, 'toc_k_min'] = toc_k_min
-            casings_df.loc[idx, 'toc_k_max'] = toc_k_max
-            casings_df.loc[idx, 'ij_min'] = ij_min
-            casings_df.loc[idx, 'ij_max'] = ij_max
+        compute_bbox(mesh_df, casings_df, nxy=nxy, maxDepth=maxDepth, is_casing=True)
 
         # ### 3. Barriers
-
-        # z
-        barriers_mod_df['k_min'] = -1
-        barriers_mod_df['k_max'] = -1
-        # xy
-        barriers_mod_df['ij_min'] = -1
-        barriers_mod_df['ij_max'] = -1
-
-        for idx, row in barriers_mod_df.iterrows():
-            
-            top, bottom  = row['top_msl'], row['bottom_msl']
-
-            # convert to indices
-            k_min, k_max = get_k_indices(mesh_df, top, bottom)
-
-            # C) xy ranges
-            ij_min, ij_max = get_ij_indices(nxy, row['n_grd_id'])
-
-            barriers_mod_df.loc[idx, 'k_min'] = k_min
-            barriers_mod_df.loc[idx, 'k_max'] = k_max
-            barriers_mod_df.loc[idx, 'ij_min'] = ij_min
-            barriers_mod_df.loc[idx, 'ij_max'] = ij_max
+        compute_bbox(mesh_df, barriers_mod_df, nxy=nxy, maxDepth=maxDepth)
