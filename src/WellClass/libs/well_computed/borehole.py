e2faa457cdaa4914ca8e9b1aba7f19ac1f88d602
hzh <hzh@equinor.com>
2023-10-15 02:11:54 +0200

    use another borehole code

--- src/WellClass/libs/well_computed/borehole.py
+++ src/WellClass/libs/well_computed/borehole.py
@@ -5,39 +5,58 @@ import pandas as pd
 def compute_borehole(casings: dict, drilling: dict) -> dict:
     '''
     Routine to compute the effective open borehole. 
-    It takes the original hole and substracts the diameter taken by cement bond.
+    It should be the casing, or the drilling if no casing is available
 
         Args:
             casings (dict): contains casing information
             drilling (dict): contains drilling information
 
         Returns:
-            borehold (dict): contains borehole information
+            borehole (dict): contains borehole information
     '''
     
     casings_df  = pd.DataFrame(casings)
     drilling_df = pd.DataFrame(drilling)
 
-    well_concat = pd.concat([casings_df, drilling_df])
-    top_z = well_concat['top_msl'].drop_duplicates().dropna().values
-    top_z.sort()
+    # drilling columns
+    drilling_columns = ['diameter_m', 'top_msl', 'bottom_msl']
+    # casing columns
+    casing_columns = ['diameter_m', 'top_msl', 'bottom_msl']
 
-    diam = 1 + well_concat['diameter_m'].max()
+    # build dataframe
+    borehole_list = []
+    for idx in drilling_df.index:
 
-    borehole = []
-    for z_value in top_z:
-        
-        z_query = well_concat.query('top_msl == @z_value')
-        min_q = z_query.query('diameter_m == diameter_m.min()')
-        if min_q.iloc[0]['diameter_m'] < diam:
-            borehole.append(min_q.iloc[0][['top_msl', 'bottom_msl', 'diameter_m']].values.tolist())
-            diam = min_q.iloc[0]['diameter_m']
+        # extract drilling data
+        row_drilling_df = drilling_df.loc[idx, drilling_columns]
 
-    borehole = np.array(borehole)
-    borehole[:-1, 1] = borehole[1:, 0]
-    # borehole[1:, 0] = borehole[:-1, 1]
+        try:
+            # extract casings data
+            row_casing_df = casings_df.loc[idx, casing_columns]
 
-    borehole_df = pd.DataFrame(data=borehole, columns=['top_msl', 'bottom_msl', 'id_m'])
+            # collect fields
+            if row_casing_df['bottom_msl'] < row_drilling_df['bottom_msl']:
+                
+                # casing
+                top_msl = max(row_drilling_df['top_msl'], row_casing_df['top_msl'])
+                bot_msl = min(row_drilling_df['bottom_msl'], row_casing_df['bottom_msl'])
+
+                # save them to list
+                borehole_list.append((top_msl,
+                                        bot_msl, 
+                                        row_casing_df['diameter_m']
+                                        ))
+
+        except Exception as e:
+
+                # save them to list
+                borehole_list.append((row_drilling_df['top_msl'],
+                                      row_drilling_df['bottom_msl'], 
+                                      row_drilling_df['diameter_m'] 
+                                      ))
+
+
+    borehole_df = pd.DataFrame(data=borehole_list, columns=['top_msl', 'bottom_msl', 'id_m'])
 
     return borehole_df.to_dict()
 
---
+++ src/WellClass/libs/well_computed/borehole_old.py
@@ -0,0 +1,43 @@
+
+import numpy as np
+import pandas as pd
+
+def compute_borehole(casings: dict, drilling: dict) -> dict:
+    '''
+    Routine to compute the effective open borehole. 
+    It takes the original hole and substracts the diameter taken by cement bond.
+
+        Args:
+            casings (dict): contains casing information
+            drilling (dict): contains drilling information
+
+        Returns:
+            borehole (dict): contains borehole information
+    '''
+    
+    casings_df  = pd.DataFrame(casings)
+    drilling_df = pd.DataFrame(drilling)
+
+    well_concat = pd.concat([casings_df, drilling_df])
+    top_z = well_concat['top_msl'].drop_duplicates().dropna().values
+    top_z.sort()
+
+    diam = 1 + well_concat['diameter_m'].max()
+
+    borehole = []
+    for z_value in top_z:
+        
+        z_query = well_concat.query('top_msl == @z_value')
+        min_q = z_query.query('diameter_m == diameter_m.min()')
+        if min_q.iloc[0]['diameter_m'] < diam:
+            borehole.append(min_q.iloc[0][['top_msl', 'bottom_msl', 'diameter_m']].values.tolist())
+            diam = min_q.iloc[0]['diameter_m']
+
+    borehole = np.array(borehole)
+    borehole[:-1, 1] = borehole[1:, 0]
+    # borehole[1:, 0] = borehole[:-1, 1]
+
+    borehole_df = pd.DataFrame(data=borehole, columns=['top_msl', 'bottom_msl', 'id_m'])
+
+    return borehole_df.to_dict()
+
