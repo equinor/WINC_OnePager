5c9b128f38f334abe0ae17c25a00157ddd14ae73
Alejandro Bello-Palacios <lord_alejo@hotmail.com>
2023-10-31 13:05:28 +0100

    New experiment that generates coarse grid

--- src/WellClass/libs/grid_utils/well_df.py
+++ src/WellClass/libs/grid_utils/well_df.py
@@ -7,12 +7,13 @@ class WellDataFrame:
         """ This is a container class
         """
 
-        # # Dataframes for drilling, casings, borehole and barriers
+        # # Dataframes for drilling, casings, borehole and geological tops
 
         self.drilling_df = pd.DataFrame(my_well.drilling)
         self.casings_df = pd.DataFrame(my_well.casings)
         self.borehole_df = pd.DataFrame(my_well.borehole)
         self.annulus_df = pd.DataFrame(my_well.annulus)
+        self.geology_df = pd.DataFrame(my_well.geology)
         
         # and for the barriers
         self.barriers_df = pd.DataFrame(my_well.barriers)
