dd6d10b4d20f2f97930df4b6b64b4edffd005070
Alejandro Bello-Palacios <49291809+Alejobep@users.noreply.github.com>
2024-05-08 12:50:14 +0000

    Include solve_ipov for integraion

--- src/WellClass/libs/well_pressure/co2_pressure.py
+++ src/WellClass/libs/well_pressure/co2_pressure.py
@@ -3,15 +3,16 @@ import numpy as np
 import pandas as pd
 
 from scipy.interpolate import RectBivariateSpline
-import scipy.constants
-from typing import Union
+import scipy.constants as const
+from typing import Union, Tuple
+from scipy.integrate import solve_ivp
 
 from ..pvt.pvt import get_hydrostatic_P, get_pvt
 from ..utils.compute_intersection import compute_intersection
 
 '''Some global parameters'''
-G       = scipy.constants.g   #9.81 m/s2 gravity acceleration
-BAR2PA  = scipy.constants.bar #10**5 Going from bars to Pascal: 1 bar = 10**5 Pascal
+G       = const.g   #9.81 m/s2 gravity acceleration
+BAR2PA  = const.bar #10**5 Going from bars to Pascal: 1 bar = 10**5 Pascal
 SHMIN_FAC = 0.1695
 
 '''Global names. Should be replaced by a global class containing these names? TODO'''
@@ -95,7 +96,7 @@ def _integrate_pressure(pt_df_in: pd.DataFrame, get_rho: callable, reference_dep
 
     ## Initialization
     #Presure at MSL
-    p_msl = scipy.constants.atm/BAR2PA  #1.01325 bar Pressure at MSL
+    p_msl = const.atm/BAR2PA  #1.01325 bar Pressure at MSL
 
     #New columns needed in DataFrame
     if colname_p not in pt_df.columns:
@@ -139,6 +140,75 @@ def _integrate_pressure(pt_df_in: pd.DataFrame, get_rho: callable, reference_dep
 
     return pt_df
 ##################################################################################   
+
+
+
+def _Pdz_odesys(z: float, y: np.ndarray, well_header: dict, temp_getter: callable, rho_getter: callable)  -> Tuple[float]:
+    P = y[0]
+    T = temp_getter(z, well_header)
+    rho = rho_getter(P, T)[0, 0]
+    dPdz = rho * const.g / const.bar
+    return dPdz,
+
+
+# Compute the temperature given the input gradient
+def compute_T(z : Union[float, int] , well_header: dict) -> float:
+    T = well_header['sf_temp'] + max(0, z - well_header['sf_depth_msl']) * (well_header['geo_tgrad'] / 1000)
+    return T
+
+
+def _integrate_pressure(well_header: dict, pt_df_in: pd.DataFrame, get_rho: callable, reference_depth: float, 
+                        reference_pressure: float, direction: str, colname_p: str) -> pd.DataFrame:
+    '''
+    Simple integration to find pressure
+    Starting point: reference_depth at reference pressure.  Reference temperature is found in pt_df
+                    Then iterate upwards (up) or downwards (down) to subtract or add pressure.
+                    Recalculates rho at each step.
+    '''
+    pt_df = pt_df_in.copy()
+
+    ## Initialization
+    #Presure at MSL
+    p_msl = const.atm/BAR2PA  #1.01325 bar Pressure at MSL
+
+    #New columns needed in DataFrame
+    if colname_p not in pt_df.columns:
+        pt_df[colname_p] = np.nan
+
+    colname_rho = colname_p+'_rho'
+    if colname_rho not in pt_df.columns:
+        pt_df[colname_rho] = np.nan
+
+    #Take out the part of the dataframe that is either below or above the reference depth.
+    if direction.lower() == "up":
+        query = pt_df.query('depth_msl<=@reference_depth')
+        query = query[::-1]
+        sign  = -1
+    elif direction.lower() == "down":
+        query = pt_df.query('depth_msl>=@reference_depth')
+        sign = 1
+    else:
+        print(f"ERROR: Not a valid direction. It should be 'up' or 'down'. You wrote {direction.lower()}")
+
+    z_final = query['depth_msl'].iloc[-1]
+
+    ###Do the calculations
+    #Starting pressure and depth
+    p0  = reference_pressure
+    z0 = reference_depth
+
+
+    solution = solve_ivp(_Pdz_odesys, [z0, z_final], [p0], args=(well_header, compute_T,get_rho), 
+                         t_eval=query['depth_msl'].values, dense_output=True,
+                         method = 'Radau')
+    
+
+    
+
+    
+    pt_df.loc[query.index, colname_p] = solution.y[0]
+
+    return pt_df
 
 def compute_MSAD(p_init: dict, pt_df: pd.DataFrame):
 
