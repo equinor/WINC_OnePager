de2593978f9b954bbe2290797fe1308e2682d598
Alejandro Bello-Palacios <49291809+Alejobep@users.noreply.github.com>
2024-05-15 12:11:39 +0000

    merge conflict

--- src/WellClass/libs/well_pressure/__init__.py
+++ src/WellClass/libs/well_pressure/__init__.py
@@ -3,9 +3,5 @@
 from .pressure import Pressure
 from .pressure_scenario import FluidP_scenario
 
-<<<<<<< HEAD
-from .barrier_pressure import compute_barrier_leakage
-=======
 from .barrier_pressure import compute_barrier_leakage
 
->>>>>>> 5531835 (Clean up and update file library)
