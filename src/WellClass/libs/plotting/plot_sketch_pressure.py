bd9a3a4b1a77f0aa6086dce4f6986a42f34c4fce
Alejandro Bello-Palacios <49291809+Alejobep@users.noreply.github.com>
2024-06-04 11:22:12 +0000

    Make plot include infor about the mixture

--- src/WellClass/libs/plotting/plot_sketch_pressure.py
+++ src/WellClass/libs/plotting/plot_sketch_pressure.py
@@ -15,7 +15,9 @@ def plot_sketch_pressure(my_well:Well, my_pressure:Pressure, *, save_file= None)
 
     plot_pressure(my_pressure, my_well.geology, my_well.barriers, ax=ax2)
     
-    fig.suptitle(f'{my_well.header["well_name"]}', fontsize=10)
+    title = f'{my_well.header["well_name"]}\n{my_pressure.mixture_name}: {my_pressure.mixture_composition}'
+
+    fig.suptitle(title, fontsize=10)
 
 
     fig.tight_layout()
