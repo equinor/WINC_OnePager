9478722afb0914a5c537a733d0c64539a72ee8d4
Alejandro Bello-Palacios <49291809+Alejobep@users.noreply.github.com>
2024-08-06 12:36:40 +0000

    Script to produce the data needed to generate the pressure plots.

--- experiments/well_pressure_tables.py
+++ experiments/well_pressure_tables.py
@@ -4,7 +4,7 @@
 
 # 1. smeaheia_v1
 
-$ python -m experiments.well_sketch_pressure --config-file ./test_data/examples/smeaheia_v1/smeaheia.yaml -pvt ./test_data/pvt_constants
+$ python -m experiments.well_pressure_tables --config-file ./test_data/examples/smeaheia_v1/smeaheia.yaml
 
 # 2. smeaheia_v2
 
@@ -25,22 +25,36 @@ import pathlib
 
 from matplotlib import pyplot as plt
 
-from src.WellClass.libs.utils import (
+from WellClass.libs.utils import (
     csv_parser,
     yaml_parser,
 )
 
 # WellClass
-from src.WellClass.libs.well_class import Well
+from WellClass.libs.well_class import Well
 
 # Pressure Class
-from src.WellClass.libs.well_pressure import Pressure
+from WellClass.libs.well_pressure import Pressure
 
 
 # plotting libraries
-from src.WellClass.libs.plotting import (
-    plot_sketch_pressure
+from WellClass.libs.plotting import (
+    plot_sketch_pressure,
+    plot_pressure
     )
+
+from importlib.resources import files
+
+
+# def parse_numbers(num):
+#     try:
+#         return int(num)
+#     except ValueError:
+#         try:
+#             return float(num)
+#         except ValueError:
+#             return [float(item) for item in num.split(',')]
+
 
 def main(args: Namespace):
 
@@ -50,7 +64,22 @@ def main(args: Namespace):
     # for example, './test_data/examples/smeaheia_v1.yaml'
     well_name = pathlib.Path(args.config_file)
 
-    pvt_path = pathlib.Path(args.pvt_db_path)
+    
+
+    # pvt_path = pathlib.Path(args.pvt_db_path)
+    mixture_index = args.mixture_index
+    print(f'{mixture_index=}')
+
+    mixtures_list = ['pure_co2', 'mixture1', 'mixture2']
+
+    mixture = mixtures_list[mixture_index]
+
+    pvt_files = files('test_data.pvt_constants')
+    pvt_path = pvt_files / mixture
+
+
+    #depth values to evaluyate pressure at Shmin
+    maxP_values = args.max_pressure_depths
 
     # extract suffix
     suffix = well_name.suffix
@@ -71,8 +100,13 @@ def main(args: Namespace):
 
     ########### 3. build Well and pressure classes ######################
 
-    # 3.1 build well class with no tables specifying geology, casings, hole size or barriers
+    # 3.1 build well class
     my_well = Well( header       = well_csv['well_header'], 
+                    # drilling     = well_csv['drilling'],
+                    # casings      = well_csv['casing_cement'],
+                    # geology      = well_csv['geology'],
+                    # barriers     = well_csv['barriers'], 
+                    # barrier_perm = well_csv['barrier_permeability'],
                     co2_datum    = well_csv['co2_datum'],
             )
     
@@ -80,6 +114,7 @@ def main(args: Namespace):
     my_pressure = Pressure( header      = well_csv['well_header'],
                             reservoir_P = well_csv['reservoir_pressure'],
                             co2_datum   = well_csv['co2_datum'],
+                            max_pressure_pos = maxP_values,
                             pvt_path    = pvt_path,)
 
 
@@ -92,8 +127,15 @@ def main(args: Namespace):
         output = os.path.join(args.out_path, args.out_name)
 
     #Plot sketch, pressures
-    plot_sketch_pressure(my_well, my_pressure, save_file=output)
+    # plot_sketch_pressure(my_well, my_pressure, save_file=output)
+    plot_pressure(my_pressure)
 
+    #Export tables to json files
+    my_pressure.pressure_CO2.to_json('pressure_co2.json', orient='records')
+
+    with open('pressure_scenarios.json', 'w') as f:
+        # Use json.dump() to write dictionary to a file
+        json.dump(my_pressure.pressure_scenarios, f)
 
     # for qc
     if not args.nodisplay:
@@ -105,13 +147,21 @@ if __name__ == '__main__':
     # Create the parser
     parser = argparse.ArgumentParser()
     
+    #yaml configuration file
     parser.add_argument('-c', "--config-file", type=str, required=True,
                         help="well configuration file, can be .yaml or .csv format")
     
 
-    parser.add_argument('-pvt', "--pvt-db-path", type=str, required=True,
-                        help="pvt fluid database path")
+   
+    #mixture index, defaulted to 0 = pure CO2
+    parser.add_argument('-mi', "--mixture-index", type=int, default=0,
+                        help="option of pre-defined chemical mixtures")
     
+    #evaluation of depth values at which max P is computed.
+    parser.add_argument('-maxP', "--max-pressure-depths", nargs='+', type=float,
+                        help="option for depths to eavluate at which CO2 gradient reaches Shmin")
+
+
     # save figure to the disk
     parser.add_argument('-p', '--out-path', type=str,
                         default='.',
@@ -129,4 +179,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     main(args)
-
