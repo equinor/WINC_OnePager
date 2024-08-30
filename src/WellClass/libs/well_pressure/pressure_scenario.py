8bbda970ce7684e0a23d0db0a281d9f3dc23cfd6
Alejandro Bello-Palacios <49291809+Alejobep@users.noreply.github.com>
2024-08-30 21:52:05 +0000

    Clean up an d improve readability

--- src/WellClass/libs/well_pressure/pressure_scenario.py
+++ src/WellClass/libs/well_pressure/pressure_scenario.py
@@ -29,17 +29,17 @@ class FluidP_scenario:
     """
     header    : dict
     ref_P     :  pd.DataFrame  #Init table with hydrostatic pressure, temperature and Shmin profile
-    rho_CO2   :  RectBivariateSpline
-    rho_H2O   :  RectBivariateSpline
-    p_name    :  str = None
-    p_delta   :  float = np.nan
-    p_resrv   :  float = np.nan
-    z_resrv   :  float = np.nan
-    p_CO2_datum : float = np.nan
-    z_CO2_datum : float = np.nan
+    rho_CO2   :  RectBivariateSpline #getter for CO2 density
+    rho_H2O   :  RectBivariateSpline #getter for H2O density
+    p_name    :  str = None #Name of the pressure scenario
+    p_delta   :  float = np.nan #Delta reservoir pressure
+    p_resrv   :  float = np.nan #Reservoir pressure at datum (z_resrv)
+    z_resrv   :  float = np.nan #Depth of reservoir datum
+    p_CO2_datum : float = np.nan #Pressure at CO2 datum
+    z_CO2_datum : float = np.nan #Depth of CO2 datum
     p_MSAD    :  float = np.nan #Pressure at Minimum Safety Abandonement depth
     z_MSAD    :  float = np.nan #Depth at Minimum Safety Abandonement depth
-    P_table   :  pd.DataFrame = None
+    P_table   :  pd.DataFrame = None #Pressure table with CO2 and H2O pressures
 
     def __repr__(self):
         p_resrv_abs = f"reservoir pressure:\t{self.p_resrv:.2f} @ {self.z_resrv} mTVDMSL"
@@ -53,6 +53,8 @@ class FluidP_scenario:
     def integrate_upwards(self):
         """
         Method to compute pressure tables starting from reservoir pressure.
+        CO2 pressure is integrated upwards from CO2 datum and input reservoir pressure (p_resrv).
+        H2O pressure is integrated upwards and downwards from CO2 datum and input reservoir pressure (p_resrv).
         """
         print(f'Pressure scenario {self.p_name}: Compute pressures assuming reservoir pressure is {self.p_resrv:.2f} bar at {self.z_resrv:.2f} mTVDMSL')
 
@@ -109,6 +111,8 @@ class FluidP_scenario:
     def integrate_downwards(self):
         """
         Method to compute maximum pressure tables starting from Shmin at given depth.
+        CO2 pressure is integrated downwards from input depth (z_MSAD). 
+        H2O pressure is integrated upwards and downwards from point where CO2 pressure meets CO2 datum.
         """
         
         print(f'Pressure scenario {self.p_name}: Compute maximum pressurization needed to reach Shmin at {self.z_MSAD} mTVDMSL')
