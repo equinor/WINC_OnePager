1f45301dd6a635c0c4544502836b7dc66f1a7598
Alejandro Bello-Palacios <49291809+Alejobep@users.noreply.github.com>
2025-04-28 11:50:59 +0000

    Add onepager function

--- src/WellClass/libs/plotting/__init__.py
+++ src/WellClass/libs/plotting/__init__.py
@@ -2,4 +2,5 @@ from .plot_pressure import plot_pressure
 from .plot_pt import plot_pt
 from .plot_sketch import plot_sketch
 from .plot_sketch_pressure import plot_sketch_pressure
+from .plot_onepager import plot_onepager
 from .plot_wellbore_elements import *
---
+++ src/WellClass/libs/plotting/plot_onepager.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt 
+from .plot_pressure import plot_pressure
+from .plot_sketch import plot_sketch
+from ..well_class.well_class import Well
+from ..well_pressure.Pressure import Pressure
+
+#Plot sketch, pressures
+
+def plot_onepager(well:Well, pressure:Pressure,
+                  #plot_sktech arguments
+                  draw_drillings=True,
+                  draw_casings=True,
+                  draw_casing_shoes=True,
+                  draw_open_hole=False,    # Note: default is False
+                  draw_welded=True,
+                  draw_cement_bond=True,
+                  draw_barriers=True,
+                  draw_geology=True,
+                  draw_annotation=True,
+                  #plot_pressure arguments
+                  geology_dict: dict = None,
+                  barriers: dict = None, 
+                  plot_HSP: bool = False, 
+                  plot_MSAD: bool = False, 
+                  legend: bool = True,
+                  plot_selected_scenarios: list = None,
+                  plot_resrv: bool = False,  # Option to plot (p_resrv, z_resrv)
+                  plot_fluid_contact: bool = False,
+                  plot_fluid_pressure: bool = True,
+                  plot_delta_p: bool = False  ):
+    """
+    Plot a one-pager of the well and pressure profile.
+    
+    Args:
+        my_well (Well): Well object containing well data.
+        my_pressure (Pressure): Pressure object containing pressure data.
+        ax (matplotlib.axes.Axes, optional): Axes to plot on. If None, a new figure is created.
+    """
+    
+    
+    # Plot pressure profile
+    fig, (ax1, ax2) = plt.subplots(1,2, sharey=True, figsize=(10, 8))
+    plot_sketch(well,  ax=ax1, 
+                draw_drillings=draw_drillings,
+                draw_casings=draw_casings,
+                draw_casing_shoes=draw_casing_shoes,
+                draw_open_hole=draw_open_hole,
+                draw_welded=draw_welded,
+                draw_cement_bond=draw_cement_bond,
+                draw_barriers=draw_barriers,
+                draw_geology=draw_geology,
+                draw_annotation=draw_annotation)
+    plot_pressure(pressure, ax=ax2, 
+                  geology_dict=geology_dict,
+                  barriers=barriers, 
+                  plot_HSP=plot_HSP, 
+                  plot_MSAD=plot_MSAD, 
+                  legend=legend,
+                  plot_selected_scenarios=plot_selected_scenarios,
+                  plot_resrv=plot_resrv,  # Option to plot (p_resrv, z_resrv)
+                  plot_fluid_contact=plot_fluid_contact,
+                  plot_fluid_pressure=plot_fluid_pressure,
+                  plot_delta_p=plot_delta_p)
+
+    fig.suptitle(f'{well.header['well_name']}')
+    fig.tight_layout()
+    fig.subplots_adjust(wspace=0)
+
+    return fig, ax1, ax2
